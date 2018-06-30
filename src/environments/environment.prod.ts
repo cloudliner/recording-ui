9e88b939b3dc48aa6fe04d6fe9b5fa6a3f107464
cloudliner <cloudliner.jp@gmail.com>
2018-06-30 17:35:12 +0900

    add firebase key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAaPIU7NiOkDd71H_eNsn_EQPnt4A4kmVI',
+    authDomain: 'chrome-recording-208807.firebaseapp.com',
+    databaseURL: 'https://chrome-recording-208807.firebaseio.com',
+    projectId: 'chrome-recording-208807',
+    storageBucket: 'chrome-recording-208807.appspot.com',
+    messagingSenderId: '474667216230'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAaPIU7NiOkDd71H_eNsn_EQPnt4A4kmVI',
+    authDomain: 'chrome-recording-208807.firebaseapp.com',
+    databaseURL: 'https://chrome-recording-208807.firebaseio.com',
+    projectId: 'chrome-recording-208807',
+    storageBucket: 'chrome-recording-208807.appspot.com',
+    messagingSenderId: '474667216230'
+  }
 };
 
 /*
